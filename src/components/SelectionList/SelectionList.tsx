e98a8daaca09be103d496f7e5b69bb997b104f3f
muhammad <m7akem@github.com>
2019-04-17 02:50:57 +0300

    Selection List almost done

--- src/components/SelectionList/SelectionList.tsx
+++ src/components/SelectionList/SelectionList.tsx
@@ -1,25 +1,47 @@
 import * as React from "react";
+import { useState } from "react";
 import "./SelectionList.css";
 
-export default function SelectionList() {
-  const style1 = {
-    background: "green"
-  }
-  const style2 = {
-    background: "blue"
-  }
+export interface IListProps {
+  list: IOption[];
+}
+
+export interface IOption {
+  name: string;
+  type?: string;
+  imageURL: string;
+}
+
+export default function SelectionList(props: IListProps) {
+  const [showAllList, setShow] = useState(true);
+  const [selectionListClasses, setClass] = useState(["SelectionList", "show"]);
+  const [selected, setSelected] = useState(0);
+  const optionClickedHandler = (index: number) => {
+    setShow(!showAllList);
+    setSelected(index);
+  };
+  React.useEffect(() => {
+    if (showAllList) {
+      console.log(showAllList);
+      setClass(["SelectionList", "show"]);
+    } else {
+      setClass(["SelectionList"]);
+    }
+  }, [showAllList]);
   return (
-    <div className="SelectionList show">
+    <div className={selectionListClasses.join(" ")}>
       <ul className="no-list-style">
-        <li className="option">
-        {/* <div /> */}
-        </li>
-        <li className="option active" style={style1}>
-        {/* <div /> */}
-        </li>
-        <li className="option" style={style2}>
-        {/* <div /> */}
-        </li>
+        {props.list.map((option: any, index: number) => {
+          return (
+            <li
+              key={index}
+              className={"option" + (selected === index ? " active" : "")}
+              onPointerDown={optionClickedHandler.bind(null, index)}
+            >
+              {option.name}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/containers/Indicitors/Indicitors.tsx
+++ src/containers/Indicitors/Indicitors.tsx
@@ -4,11 +4,18 @@ import SelectionList from "../../components/SelectionList";
 import "./Indicitors.css";
 
 class Indicitors extends React.Component {
+  public cities = [
+      { name: "riyadh", imageURL: "riyadh", type: "City" },
+      { name: "makkah", imageURL: "makkah", type: "City" },
+      { name: "madinah", imageURL: "madinah", type: "City" },
+      { name: "jeddah", imageURL: "jeddah", type: "City" },
+      { name: "dammam", imageURL: "dammam", type: "City" }
+    ]
   public render() {
     return (
       <div className="Indicitors">
         <div className="select-city">
-        <SelectionList/>
+        <SelectionList list={this.cities}/>
         </div>
         <div className="toolbar">{/* TODO */}</div>
         <div className="list-of-indicitors">
